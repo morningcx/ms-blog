b3e42d9452725903d7ea7a1e7457fd7db4031f06
qwer <35294458+xx877699@users.noreply.github.com>
2019-02-03 13:41:44 +0800

    Initial commit

---
+++ src/main/java/com/morningcx/ms/blog/MsBlogApplication.java
@@ -0,0 +1,17 @@
+package com.morningcx.ms.blog;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class MsBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBlogApplication.class, args);
+        log.info("========================启动成功========================");
+    }
+
+}
+
