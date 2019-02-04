cd71e60ae2262c3bcfbfd177b76756b9dcebddf8
qwer <35294458+xx877699@users.noreply.github.com>
2019-02-04 12:38:10 +0800

    添加editormd.js，md保存，md转html

--- src/main/java/com/morningcx/ms/blog/controller/ArticleController.java
+++ src/main/java/com/morningcx/ms/blog/controller/ArticleController.java
@@ -2,14 +2,18 @@ package com.morningcx.ms.blog.controller;
 
 import com.morningcx.ms.blog.entity.Article;
 import com.morningcx.ms.blog.jpa.ArticleJpa;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author guochenxiao
  * @date 2019/2/3
  */
+@Slf4j
 @RestController
 public class ArticleController {
     @Autowired
@@ -21,4 +25,14 @@ public class ArticleController {
         return article;
     }
 
+    @GetMapping("getArticle")
+    public Article getArticle(@RequestParam Integer id) {
+        return articleJpa.findById(id).orElse(null);
+    }
+
+    @PostMapping("updateArticle")
+    public Article updateArticle(Article article, String test) {
+        log.info(test);
+        return articleJpa.save(article);
+    }
 }
